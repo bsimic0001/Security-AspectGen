40c46a530c3f008f61accdedabd03b7ba50a8d67
Bojan Simic <bsimic0001@gmail.com>
2012-01-20 15:32:50 -0500

    commiting data parser.

--- Thesis/src/com/thesis/aop/data/DataFileParser.java
+++ Thesis/src/com/thesis/aop/data/DataFileParser.java
@@ -34,8 +34,10 @@ public class DataFileParser {
 				boolean newFileName = false;
 				boolean newFilePath = false;
 				boolean newLineStart = false;
+				boolean newSourceIssue = false;
 
 				Issue tempIssue;
+				Issue tempSourceIssue;
 
 				public void startElement(String uri, String localName,
 						String qName, Attributes attributes)
@@ -47,6 +49,12 @@ public class DataFileParser {
 						tempIssue = new Issue();
 						tempIssue.setRuleID(attributes.getValue("ruleID"));
 						tempIssue.setIid(attributes.getValue("iid"));
+					}
+					if(qName.equalsIgnoreCase("SOURCE")){
+						newSourceIssue = true;
+						tempSourceIssue = new Issue();
+						tempSourceIssue.setRuleID("Source_" + tempIssue.getRuleID());
+						tempSourceIssue.setIid("Source_" + tempIssue.getIid());
 					}
 					if(qName.equalsIgnoreCase("CATEGORY")){
 						newCategory = true;
@@ -81,6 +89,10 @@ public class DataFileParser {
 						issues.add(tempIssue);
 						newIssue = false;
 					}
+					if (qName.equalsIgnoreCase("SOURCE")) {
+						issues.add(tempSourceIssue);
+						newSourceIssue = false;
+					}
 					// System.out.println("End Element :" + qName);
 
 				}
@@ -90,34 +102,53 @@ public class DataFileParser {
 
 					if(newCategory){
 						tempIssue.setCategory(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setCategory(new String(ch, start, length));
 						newCategory = false;
 					}
 					if(newFolder){
 						tempIssue.setFolder(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setFolder(new String(ch, start, length));
 						newFolder = false;
 					}
 					if(newKingdom){
 						tempIssue.setKingdom(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setKingdom(new String(ch, start, length));
 						newKingdom = false;
 					}
 					if(newAbstract){
 						tempIssue.setAbstractDefn(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setAbstractDefn("SOURCE NODE - NO ABSTRACT AVAILABLE");
 						newAbstract = false;
 					}
 					if(newPriority){
 						tempIssue.setPriority(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setPriority(new String(ch, start, length));
 						newPriority = false;
 					}
 					if(newFileName){
-						tempIssue.setFileName(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setFileName(new String(ch, start, length));
+						else
+							tempIssue.setFileName(new String(ch, start, length));
 						newFileName = false;
 					}
 					if(newFilePath){
-						tempIssue.setFilePath(new String(ch, start, length));
+						if(newSourceIssue)
+							tempSourceIssue.setFilePath(new String(ch, start, length));
+						else
+							tempIssue.setFilePath(new String(ch, start, length));
 						newFilePath = false;
 					}
 					if(newLineStart){
-						tempIssue.setLineStart(Integer.valueOf(new String(ch, start, length)).intValue());
+						if(newSourceIssue)
+							tempSourceIssue.setLineStart(Integer.valueOf(new String(ch, start, length)).intValue());
+						else
+							tempIssue.setLineStart(Integer.valueOf(new String(ch, start, length)).intValue());
 						newLineStart = false;
 					}
 				}
