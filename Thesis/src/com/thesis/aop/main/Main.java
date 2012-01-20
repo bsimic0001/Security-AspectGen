78583f88e01cdc8c2340430d9b525f7d3603c9a5
Bojan Simic <bsimic0001@gmail.com>
2012-01-20 17:03:13 -0500

    implemented code beautifier.

--- Thesis/src/com/thesis/aop/main/Main.java
+++ Thesis/src/com/thesis/aop/main/Main.java
@@ -1,8 +1,10 @@
 package com.thesis.aop.main;
 
+import java.io.IOException;
+
 import com.thesis.aop.data.DataFileParser;
 import com.thesis.aop.data.FunctionsParser;
-
+import net.barenca.jastyle.JAStyleMain;
 public class Main {
 
 	/**
@@ -15,6 +17,15 @@ public class Main {
 		parser.ParseFile("jada_report.xml");
 		FunctionsParser functionsParser = new FunctionsParser();
 		functionsParser.ParseFile("xssfunctions.xml");
+		
+		String[] files = new String[1];
+		files[0] = System.getProperty("user.dir") + "/aspects/test.java";
+		try {
+			JAStyleMain.ParseFiles(files);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
